50a7eb865e51b7905017adfcefa274da0197227f
Miheer Tamkhane <sarangtamkhane10@gmail.com>
2024-08-13 17:10:42 +0530

    updated the configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  output: "server",
   integrations: [mdx(), sitemap(), tailwind()],
+  adapter: netlify(),
   devToolbar: {
     enabled: false
   }
