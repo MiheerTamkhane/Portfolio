8395f63b9e2e731eb6f8e75c45ff174407fe74e6
Miheer Tamkhane <sarangtamkhane10@gmail.com>
2024-08-29 14:57:13 +0530

    some things

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
-  output: "server",
+  site: "https://astro-nano-demo.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
   devToolbar: {
     enabled: false
   }
 });
+
